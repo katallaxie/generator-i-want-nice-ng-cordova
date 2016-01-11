ee43e0469995affdd8412c5dd56516aa8ae74588
Sebastian Döll <sebastian@pixelmilk.com>
2016-01-11 23:31:21 +0100

    [Feature] Icons and splash production

---
+++ templates/app/grunt/icons.js
@@ -0,0 +1,14 @@
+/**
+ * Grunt - cordova-icons (https://github.com/katallaxie/grunt-cordova-icons.git)
+ */
+module.exports = (grunt, config) => {
+  // returning the config object
+  return {
+    all: {
+      files:
+        src: './icon.png',
+        dest: 'build/res'
+      }]
+    }
+  }
+}
---
+++ templates/app/grunt/splashs.js
@@ -0,0 +1,14 @@
+/**
+ * Grunt - cordova-splashs (https://github.com/katallaxie/grunt-cordova-splashs.git)
+ */
+module.exports = (grunt, config) => {
+  // returning the config object
+  return {
+    all: {
+      files:
+        src: './splash.png',
+        dest: 'build/res'
+      }]
+    }
+  }
+}
